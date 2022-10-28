c24370130126915c13892dd5f8522250fa9c6481
Js-iT <66570858+jakirsoftware@users.noreply.github.com>
2022-10-28 13:05:21 +0600

    login

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php session_start(); /* Starts the session */
+
+if(!isset($_SESSION['UserData']['Username'])){
+	header("location:login.php");
+	exit;
+}
+?>
+
+
+
+
+
+
+<body>
+	<iframe src="
+	
+	
+https://docs.google.com/spreadsheets/d/e/2PACX-1vRzA-d1sm7YEXa0783tLQJ1blyJUUrh3bo1-lvqzYHmmTpKGmNlq-Yu9aEJ9NQhwN6Va0aclYXTWO_v/pubhtml?gid=0&single=true
+	
+	
+	
+	
+	
+	
+	"
+			frameborder="0"
+			marginheight="0"
+			marginwidth="0"
+			width="100%"
+			height="100%"
+			scrolling="auto">
+</iframe>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,58 @@
+<?php session_start(); /* Starts the session */
+	
+	/* Check Login form submitted */	
+	if(isset($_POST['Submit'])){
+		/* Define username and associated password array */
+		$logins = array('js' => 'it','username1' => 'password1','username2' => 'password2');
+		
+		/* Check and assign submitted Username and Password to new variable */
+		$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+		$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+		
+		/* Check Username and Password existence in defined array */		
+		if (isset($logins[$Username]) && $logins[$Username] == $Password){
+			/* Success: Set session variables and redirect to Protected page  */
+			$_SESSION['UserData']['Username']=$logins[$Username];
+			header("location:index.php");
+			exit;
+		} else {
+			/*Unsuccessful attempt: Set error message */
+			$msg="<span style='color:red'>Invalid Login Details</span>";
+		}
+	}
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="./style.css" rel="stylesheet">
+<h1><marquee behavior="alternate" direction="left" width="100%" direction="left" height="100px">Welcome: Jakir Software (Call: +8801631590346)</marquee></h1>
+</head>
+<body>
+<br>
+<form action="" method="post" name="Login_Form">
+  <table width="400" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+    <?php if(isset($msg)){?>
+    <tr>
+      <td colspan="2" align="center" valign="top"><?php echo $msg;?></td>
+    </tr>
+    <?php } ?>
+    <tr>
+      <td colspan="2" align="left" valign="top"><h3>Login</h3></td>
+    </tr>
+    <tr>
+      <td align="right" valign="top">Username</td>
+      <td><input name="Username" type="text" class="Input"></td>
+    </tr>
+    <tr>
+      <td align="right">Password</td>
+      <td><input name="Password" type="password" class="Input"></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+    </tr>
+  </table>
+</form>
+</body>
+</html>
